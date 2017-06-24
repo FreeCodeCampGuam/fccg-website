ec28616c9fe1d8d209eeb5820541d563640d1aab
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-06-24 13:07:44 -0700

    implement Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => (
+  <div id="navbar">
+    <ul>
+      <li><a href="#Events">Events</a></li>
+      <li><a href="#Meetups">Meetups</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#Contact">Contact</a></li>
+    </ul>
+  </div>
+);
+
+export default Navbar;
